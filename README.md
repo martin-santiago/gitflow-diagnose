f5a954556d4159afaca0569aecd75c792bf6abc8
martin-santiago <mprez@uc.cl>
2022-08-13 22:06:06 -0400

    📝 Readme edit and syntax

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-Perdí mucho tiempo intentando leer el archivo y solo alcance a hacer top_retweeted 
+Perdí mucho tiempo intentando leer el archivo y solo alcance a hacer top_retweeted;
+Para ejecutar hay que añadir el archivo del dataset al folder, y ejecutar con `python3 main.py`
--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from "top_days.py" import top_days 
-from "top_hashtags.py" import top_hashtags 
-from "top_retweeted.py" import top_retweeted 
-from "top_users.py" import top_users 
+from top_retweeted import top_retweeted 
 
-def main:
+
+def main():
     top_retweeted()
     #top_days()
     #top_users()
